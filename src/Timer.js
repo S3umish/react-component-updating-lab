8a205dd9987cd006544db296eae5a2dc40b92b46
sunitee <suniteebajpai2@gmail.com>
2021-05-26 10:55:52 -0400

    commit tree 22aa178ba2ca24f7adc4cfb4f82abb42600e5ba3

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,7 @@ class Timer extends Component {
   constructor() {
     super();
     this.timer = React.createRef();
+   
     this.state = {
       time: 0,
       color: "#" + Math.floor(Math.random() * 16777215).toString(16)
@@ -12,11 +13,27 @@ class Timer extends Component {
 
   //Your code here
 
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.state.time === nextState.time){
+      return false
+    }
+    return true
+  }
+
+  componentDidUpdate(){
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16)
+    console.log(this.timer.current)
+    console.log(this.timer.current.style.background);
+    this.timer.current.style.width = 240+this.state.time*5/1000+"px"
+    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
+  }
+
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
       this.props.updateInterval * 1000
-    );
+      
+    )
   }
 
   componentWillUnmount() {
@@ -27,12 +44,15 @@ class Timer extends Component {
     const { time, color, logText } = this.state;
     return (
       <section className="Timer" style={{ background: color }} ref={this.timer}>
+        
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
         <aside className="logText">{logText}</aside>
         <small onClick={this.handleClose}>X</small>
       </section>
+      
     );
+    
   }
 
   clockTick = () => {
